fa29a4cd85b4a508f8d05bafb6c11c48b7f60f88
Kacie Guthrie <kacieguthrie@gmail.com>
2018-04-30 12:22:17 -0700

    first attempt

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function onReady() {
   const toDos = [];  //define a varialbe that is an empty array
   const addToDoForm = document.getElementById('addToDoForm'); //get the form
+  let toDoId = 0;
 
   function  createNewToDo() {     //this function adds the input to the array (storing the data)
       const newToDoText = document.getElementById('newToDoText');  //get the input
@@ -9,6 +10,7 @@ function onReady() {
       toDos.push({  //add the value of the input to the toDos array (is this actualy an array or just an object?)
         title: newToDoText.value, //create a property called "title" and assign it a value of the value of the newToDo Text input
         complete: false //? what is this?
+        id: toDoId++; //assign a consecutive number to each new li (just like an array)?
       });
       newToDoText.value = ''; //clear the input
 
@@ -23,12 +25,17 @@ function onReady() {
     toDos.forEach(function(toDo){  //for each toDo element of the toDos array do the following:
       const newLi = document.createElement('li');  //create a new li
       const checkbox = document.createElement('input'); //create a new checkbox
-      checkbox.type = "checkbox";  //set checkbox type to "checkbox"
+      checkbox.type = 'checkbox';  //set checkbox type to "checkbox"
+
+      const deleteButton = document.createElement('button'); //make the delete button
+      deleteButton.type = 'button'; //set type to button
+      deleteButton.innerHTML = 'Delete'; //write delete text to button element
 
       newLi.textContent = toDo.title; //write the title of each array element to a new li
 
       toDoList.appendChild(newLi); //attach the li to the ul
       newLi.appendChild(checkbox); //attach the checkbox to the li
+      newLi.appendChild(deleteButton); //attach the delete button to the li
     });
   }
 
@@ -37,7 +44,11 @@ function onReady() {
     createNewToDo(); //run the function that adds the input to the array
   });
 
-  renderTheUI(); //what is this doing? renderTheUI doesn't have any to-dos to display in the initial state. It seems like everything still works if I comment it out. 
+  deleteButton.addEventListener('click', event => {
+    toDos.filter(toDo.id === toDoId) // not sure what to do here, just a guess
+  })
+
+  renderTheUI(); //what is this doing?
 }
 window.onload = function () { //??
   onReady();
