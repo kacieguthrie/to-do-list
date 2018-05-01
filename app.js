21701bb04a2ed5577c166bb642a83292603f6d2b
Kacie Guthrie <kacieguthrie@gmail.com>
2018-05-01 14:55:34 -0700

    delete button works

--- app.js
+++ app.js
@@ -1,55 +1,61 @@
 function onReady() {
-  const toDos = [];  //define a varialbe that is an empty array
-  const addToDoForm = document.getElementById('addToDoForm'); //get the form
-  let id = 0;
+  let toDos = [];
+  const addToDoForm = document.getElementById('addToDoForm');
+  let id = 0; //why isn't this defined in createNewToDo()?
 
   function  createNewToDo() {     //this function adds the input to the array (make a place to store the data)
-      const newToDoText = document.getElementById('newToDoText');  //get the input
+      const newToDoText = document.getElementById('newToDoText');
       if (!newToDoText.value) {return;}  //check that the input has a truthy value
 
-      toDos.push({  //add the value of the input to the toDos array (is this actualy an array or just an object?)
-        title: newToDoText.value, //create a property called "title" and assign it a value of the value of the newToDo Text input
-        id: id++, //assign a consecutive number to each new li (just like an array)?
+      toDos.push({ 
+        title: newToDoText.value,
+        id: id++ //assign a consecutive number to each new li (just like an array index)
       });
-      newToDoText.value = ''; //clear the input
+      newToDoText.value = '';
 
-      renderTheUI(); //make the new elements and attach them to the right places
+      renderTheUI();
+    }
+
+  function deleteToDo(id) {
+      return toDos.filter(toDo => toDo.id !== id); //this filter says: compare each element in the toDos array and retrun all that don't match the value of the id
     }
 
   function renderTheUI() {  //define function that makes the new elements and attaches them to the right places
-    const toDoList = document.getElementById('toDoList'); //get the ul
+    const toDoList = document.getElementById('toDoList');
 
-    toDoList.textContent = ''; //write an empty string to the ul
+    toDoList.textContent = '';
 
     toDos.forEach(function(toDo){  //for each toDo element of the toDos array do the following:
-      const newLi = document.createElement('li');  //create a new li
-      const checkbox = document.createElement('input'); //create a new checkbox
-      checkbox.type = 'checkbox';  //set checkbox type to "checkbox"
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
 
       const deleteButton = document.createElement('button'); //make the delete button
       deleteButton.type = 'button'; //set type to button
       deleteButton.innerHTML = 'Delete'; //write delete text to button element
 
-      newLi.textContent = toDo.title; //write the title of each array element to a new li
+      newLi.textContent = toDo.title;
 
-      toDoList.appendChild(newLi); //attach the li to the ul
-      newLi.appendChild(checkbox); //attach the checkbox to the li
-      newLi.appendChild(deleteButton); //attach the delete button to the li
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
+
+      deleteButton.addEventListener('click', event => {
+        toDos = deleteToDo(toDo.id); //use the id associated with the deleteButton parent li as the argument for the deleteToDo function and repopulate the toDos array with those values that return true.
+        renderTheUI(); //make it so using the updated toDos array.
+      });
     });
   }
 
-  addToDoForm.addEventListener('submit',event => { //listen for the 'submit' event (why curly brackets? are we defining an anon function?)
-    event.preventDefault(); //prevent a new page from opening
-    createNewToDo(); //run the function that adds the input to the array
-  });
-
-  deleteButton.addEventListener('click', event => {
-    let newToDos = toDos.filter(toDo => toDo.id !== id);
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
   });
 
   renderTheUI();
 }
-window.onload = function () {
+
+window.onload = function () { //what is this for?
   onReady();
 
 };
